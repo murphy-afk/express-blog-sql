de81185b8599d4aa7c42a789139ec8a899523bfa
murphy-afk <bardarenoemi@gmail.com>
2026-01-16 16:46:23 +0100

    feat: destroy

--- app.js
+++ app.js
@@ -47,11 +47,24 @@ app.get('/posts/:id', (req, res) => {
       return res.status(500).json({ error: 'query failed' })
     }
     if (results.length === 0) {
-      return res.status(404).json({ error: 'post not funny' })
+      return res.status(404).json({ error: 'post not found' })
     }
     res.json(results[0])
   })
-
+})
+// DESTROY
+app.delete('/posts/:id', (req, res) => {
+  const id = req.params.id
+  const sql = "DELETE FROM posts WHERE id = ?";
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json(err)
+    }
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'post not found' })
+    }
+    res.status(204).send('post deleted')
+  })
 })
 
 app.listen(port, () => {
