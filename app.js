9764e874f830a2a761589b91b5ef8127913134ea
murphy-afk <bardarenoemi@gmail.com>
2026-01-16 16:35:51 +0100

    feat: show

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import mysql from "mysql2";
 
 const app = express();
-const port = 3000;
+const port = 3006;
 app.use(express.json());
 
 const connection = mysql.createConnection({
@@ -24,10 +24,9 @@ connection.connect((err) => {
 app.get('/', (req, res) => {
   res.send('sever works')
 })
-
+// INDEX
 app.get('/posts', (req, res) => {
   const query = 'SELECT * FROM posts';
-
   connection.query(query, (err, result) => {
     if (err) {
       console.log(err);
@@ -38,6 +37,21 @@ app.get('/posts', (req, res) => {
       })
     }
   })
+})
+// SHOW
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id
+  const sql = 'SELECT * FROM posts WHERE id = ?';
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: 'query failed' })
+    }
+    if (results.length === 0) {
+      return res.status(404).json({ error: 'post not funny' })
+    }
+    res.json(results[0])
+  })
+
 })
 
 app.listen(port, () => {
